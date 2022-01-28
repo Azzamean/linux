718903a4d0e83cb9d95ef7b4d69ebc10fd9d0238
Andrew Bringaze <andrewb@proximabiz.com>
2022-01-28 22:46:04 +0000

    Linux Foundation Projects Banner

--- wp-content/themes/salient-child/functions.php
+++ wp-content/themes/salient-child/functions.php
@@ -1,18 +1,18 @@
-<?php 
-
+<?php
 require_once 'vc_addons/recent_posts_basic.php';
-   
-   
-add_action( 'wp_enqueue_scripts', 'salient_child_enqueue_styles', 100);
 
-function salient_child_enqueue_styles() {
-		
-		$nectar_theme_version = nectar_get_theme_version();
-		wp_enqueue_style( 'salient-child-style', get_stylesheet_directory_uri() . '/style.css', '', $nectar_theme_version );
-		
-    if ( is_rtl() ) {
-   		wp_enqueue_style(  'salient-rtl',  get_template_directory_uri(). '/rtl.css', array(), '1', 'screen' );
-		}
+add_action('wp_enqueue_scripts', 'salient_child_enqueue_styles', 100);
+
+function salient_child_enqueue_styles()
+{
+
+    $nectar_theme_version = nectar_get_theme_version();
+    wp_enqueue_style('salient-child-style', get_stylesheet_directory_uri() . '/style.css', '', $nectar_theme_version);
+
+    if (is_rtl())
+    {
+        wp_enqueue_style('salient-rtl', get_template_directory_uri() . '/rtl.css', array() , '1', 'screen');
+    }
 }
 
 // TOP LINUX FOUNDATION PROJECTS HEADER BANNER STRIP
@@ -28,40 +28,13 @@ function lf_meta_header()
 }
 add_action('nectar_hook_after_body_open', 'lf_meta_header', 10, 0);
 
-
-// ALLOW MIME TYPE UPLOADS; EXAMPLE: SVG's 
-function cc_mime_types($mimes) {
-  $mimes['svg'] = 'image/svg+xml';
-  $mimes['svgz'] = 'image/svg+xml';
-  return $mimes;
+// ALLOW MIME TYPE UPLOADS; EXAMPLE: SVG's
+function cc_mime_types($mimes)
+{
+    $mimes['svg'] = 'image/svg+xml';
+    $mimes['svgz'] = 'image/svg+xml';
+    return $mimes;
 }
-add_filter( 'upload_mimes', 'cc_mime_types' );
-
-
-function print_post_title() {
-global $post;
-$thePostID = $post->ID;
-$post_id = get_post($thePostID);
-$title = $post_id->post_title;
-$perm = get_permalink($post_id);
-$post_keys = array(); $post_val = array();
-$post_keys = get_post_custom_keys($thePostID);
- 
-if (!empty($post_keys)) {
-foreach ($post_keys as $pkey) {
-if ($pkey=='external_url') {
-$post_val = get_post_custom_values($pkey);
-}
-}
-if (empty($post_val)) {
-$link = $perm;
-} else {
-$link = $post_val[0];
-}
-} else {
-$link = $perm;
-}
-echo '<h2><a href="'.$link.'" rel="bookmark" title="'.$title.'">'.$title.'</a></h2>';
-}
+add_filter('upload_mimes', 'cc_mime_types');
 
 ?>
