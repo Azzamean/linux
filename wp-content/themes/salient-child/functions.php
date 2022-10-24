941fb54b0052cc94662a21f32d90f9936b8ce309
Andrew Bringaze <abringaze@linuxfoundation.org>
2022-10-24 17:30:07 +0000

    banner changes

--- wp-content/themes/salient-child/functions.php
+++ wp-content/themes/salient-child/functions.php
@@ -1,13 +1,13 @@
 <?php
 
 /* Osano code for tracking */
-add_action('wp_head', 'osano_script');
-function osano_script(){
-?>
+add_action("wp_head", "osano_script");
+function osano_script()
+{
+    ?>
 <script src="https://cmp.osano.com/16A0DbT9yDNIaQkvZ/3b49aaa9-15ab-4d47-a8fb-96cc25b5543c/osano.js"></script>
 <?php
-};
-
+}
 
 // GET WP BAKERY CUSTOM MODULES
 require_once "vc-addons/recent-posts-linux.php";
@@ -21,17 +21,80 @@ require_once "custom-post-types/projects.php";
 function salient_child_enqueue_styles()
 {
     $nectar_theme_version = nectar_get_theme_version();
-    wp_enqueue_style("salient-child-style", get_stylesheet_directory_uri() . "/style.css", "", $nectar_theme_version);
-    wp_enqueue_style("vc-addons-style", get_stylesheet_directory_uri() . "/vc-addons/vc-addons.css", "", $nectar_theme_version);
-    wp_enqueue_style("templates-style", get_stylesheet_directory_uri() . "/templates/css/templates.css", "", $nectar_theme_version);
-    wp_enqueue_style("fonts-style", get_stylesheet_directory_uri() . "/fonts/fonts.css", "", $nectar_theme_version);
+    wp_enqueue_style(
+        "salient-child-style",
+        get_stylesheet_directory_uri() . "/style.css",
+        "",
+        $nectar_theme_version
+    );
+    wp_enqueue_style(
+        "vc-addons-style",
+        get_stylesheet_directory_uri() . "/vc-addons/vc-addons.css",
+        "",
+        $nectar_theme_version
+    );
+    wp_enqueue_style(
+        "templates-style",
+        get_stylesheet_directory_uri() . "/templates/css/templates.css",
+        "",
+        $nectar_theme_version
+    );
+    wp_enqueue_style(
+        "fonts-style",
+        get_stylesheet_directory_uri() . "/fonts/fonts.css",
+        "",
+        $nectar_theme_version
+    );
     //wp_enqueue_script('bringaze-font-awesome', 'https://kit.fontawesome.com/8511f9d0cf.js', false);
-    if (is_rtl())
-    {
-        wp_enqueue_style("salient-rtl", get_template_directory_uri() . "/rtl.css", [], "1", "screen");
+    if (is_rtl()) {
+        wp_enqueue_style(
+            "salient-rtl",
+            get_template_directory_uri() . "/rtl.css",
+            [],
+            "1",
+            "screen"
+        );
     }
 }
 add_action("wp_enqueue_scripts", "salient_child_enqueue_styles", 100);
+
+// GRABS SPECIFIC SUBSITES
+if (is_multisite()) {
+    $site_id = get_current_blog_id();
+    switch ($site_id) {
+        // O3D
+        case "13":
+            require_once "sites/o3d/functions.php";
+            break;
+    }
+}
+
+// TOP LINUX FOUNDATION PROJECTS HEADER BANNER STRIP
+function lf_meta_header()
+{
+    $site_id = get_current_blog_id();
+
+    if ($site_id == "14") {
+        echo '
+  	<div class="lfprojects color">
+		<div class="container">
+			<a href="https://www.linuxfoundation.org/projects" target="_blank" rel="noopener noreferrer">
+			<img src="/wp-content/uploads/banners/lfprojects_banner_color.png">
+			</a>
+		</div>
+	</div>';
+    } else {
+        echo '
+  	<div class="lfprojects">
+		<div class="container">
+			<a href="https://www.linuxfoundation.org/projects" target="_blank" rel="noopener noreferrer">
+			<img src="/wp-content/uploads/banners/lfprojects_banner.png">
+			</a>
+		</div>
+	</div>';
+    }
+}
+add_action("nectar_hook_after_body_open", "lf_meta_header", 10, 0);
 
 // CUSTOM POST TYPE TEMPLATES FROM A DIRECTORY
 function custom_post_types_templates($template)
@@ -41,15 +104,21 @@ function custom_post_types_templates($template)
     $defaultWebinarsTemplate = locate_template("templates/single-webinars.php");
     $templateSlug = get_page_template_slug(get_queried_object_id()); // this is null if no template name is given; hence default
     // PROJECTS
-    if (is_singular("projects") && $defaultProjectsTemplate != "" && $templateSlug == null)
-    {
+    if (
+        is_singular("projects") &&
+        $defaultProjectsTemplate != "" &&
+        $templateSlug == null
+    ) {
         //if (is_singular($post_types) && !file_exists(get_stylesheet_directory() . "/single-projects.php") && get_page_template_slug(get_queried_object_id()) == null)
         $template = $defaultProjectsTemplate;
     }
     //if (is_post_type_archive($post_types) && !file_exists(get_stylesheet_directory() . "/archive-projects.php")){}
     // WEBINARS
-    if (is_singular("webinars") && $defaultWebinarsTemplate != "" && $templateSlug == null)
-    {
+    if (
+        is_singular("webinars") &&
+        $defaultWebinarsTemplate != "" &&
+        $templateSlug == null
+    ) {
         $template = $defaultWebinarsTemplate;
     }
     return $template;
@@ -59,26 +128,12 @@ add_filter("template_include", "custom_post_types_templates");
 // REDIRECT PANTHEON LOGIN TO WORK CORRECTLY
 function redirect_pantheon_login()
 {
-    if (strpos($_SERVER["REQUEST_URI"], "/wp-signup.php?") !== false)
-    {
+    if (strpos($_SERVER["REQUEST_URI"], "/wp-signup.php?") !== false) {
         wp_redirect("/wp-admin/");
         exit();
     }
 }
 add_action("init", "redirect_pantheon_login");
-
-// TOP LINUX FOUNDATION PROJECTS HEADER BANNER STRIP
-function lf_meta_header()
-{
-    echo '
-  	<div class="lfprojects">
-		<div class="container">
-			<a href="https://www.linuxfoundation.org/projects" target="_blank" rel="noopener noreferrer"><img src="https://live-lfprojects3.linuxfoundation.org/wp-content/uploads/2022/01/lfprojects_banner.png"></a>
-		</div>
-	</div>
-';
-}
-add_action("nectar_hook_after_body_open", "lf_meta_header", 10, 0);
 
 // ALLOW MIME TYPE UPLOADS; EXAMPLE: SVG's
 function cc_mime_types($mimes)
@@ -90,13 +145,14 @@ function cc_mime_types($mimes)
 add_filter("upload_mimes", "cc_mime_types");
 
 // REMOVE COMMENTS FUNCTION; CAN BE PLACED IN A MU PLUGIN IF WANTED
-add_action("admin_init", function ()
-{
+add_action("admin_init", function () {
     // REDIRECT USERS TRYING TO ACCESS COMMENTS PAGE
     global $pagenow;
 
-    if ($pagenow === "edit-comments.php" || $pagenow === "options-discussion.php")
-    {
+    if (
+        $pagenow === "edit-comments.php" ||
+        $pagenow === "options-discussion.php"
+    ) {
         wp_redirect(admin_url());
         exit();
     }
@@ -105,10 +161,8 @@ add_action("admin_init", function ()
     remove_meta_box("dashboard_recent_comments", "dashboard", "normal");
 
     // DISABLE SUPPORT FOR COMMENTS AND TRACKBACKS IN POST TYPES
-    foreach (get_post_types() as $post_type)
-    {
-        if (post_type_supports($post_type, "comments"))
-        {
+    foreach (get_post_types() as $post_type) {
+        if (post_type_supports($post_type, "comments")) {
             remove_post_type_support($post_type, "comments");
             remove_post_type_support($post_type, "trackbacks");
         }
@@ -123,15 +177,13 @@ add_filter("pings_open", "__return_false", 20, 2);
 add_filter("comments_array", "__return_empty_array", 10, 2);
 
 // REMOVE COMMENTS PAGE AND OPTION PAGE IN MENU
-add_action("admin_menu", function ()
-{
+add_action("admin_menu", function () {
     remove_menu_page("edit-comments.php");
     remove_submenu_page("options-general.php", "options-discussion.php");
 });
 
 // REMOVE COMMENTS LINK FROM ADMIN BAR
-add_action("add_admin_bar_menus", function ()
-{
+add_action("add_admin_bar_menus", function () {
     remove_action("admin_bar_menu", "wp_admin_bar_comments_menu", 60);
 });
 
@@ -142,8 +194,7 @@ function remove_toolbar_items($bar)
     // global $wp_admin_bar;
     // $wp_admin_bar->remove_node( 'blog-1-c' );
     $sites = get_blogs_of_user(get_current_user_id());
-    foreach ($sites as $site)
-    {
+    foreach ($sites as $site) {
         $bar->remove_node("blog-{$site->userblog_id}-c");
     }
 }
@@ -169,21 +220,24 @@ return $name;
 
 function salient_redux_custom_fonts()
 {
-    return ["Custom Fonts" => ["BwModelica-Bold" => "BwModelica-Bold", "BwModelica-BoldItalic" => "BwModelica-BoldItalic", "BwModelica-ExtraBold" => "BwModelica-ExtraBold", "BwModelica-ExtraBoldItalic" => "BwModelica-ExtraBoldItalic", "BwModelica-Light" => "BwModelica-Light", "BwModelica-LightItalic" => "BwModelica-LightItalic", "BwModelica-Medium" => "BwModelica-Medium", "BwModelica-MediumItalic" => "BwModelica-MediumItalic", "BwModelica-Regular" => "BwModelica-Regular", "BwModelica-RegularItalic" => "BwModelica-RegularItalic", "BwModelicaSS02-Regular" => "BwModelicaSS02-Regular", "BwModelicaSS02-RegularItalic" => "BwModelicaSS02-RegularItalic", ], ];
+    return [
+        "Custom Fonts" => [
+            "BwModelica-Bold" => "BwModelica-Bold",
+            "BwModelica-BoldItalic" => "BwModelica-BoldItalic",
+            "BwModelica-ExtraBold" => "BwModelica-ExtraBold",
+            "BwModelica-ExtraBoldItalic" => "BwModelica-ExtraBoldItalic",
+            "BwModelica-Light" => "BwModelica-Light",
+            "BwModelica-LightItalic" => "BwModelica-LightItalic",
+            "BwModelica-Medium" => "BwModelica-Medium",
+            "BwModelica-MediumItalic" => "BwModelica-MediumItalic",
+            "BwModelica-Regular" => "BwModelica-Regular",
+            "BwModelica-RegularItalic" => "BwModelica-RegularItalic",
+            "BwModelicaSS02-Regular" => "BwModelicaSS02-Regular",
+            "BwModelicaSS02-RegularItalic" => "BwModelicaSS02-RegularItalic",
+        ],
+    ];
 }
-add_filter("redux/salient_redux/field/typography/custom_fonts", "salient_redux_custom_fonts");
-
-/* Grabs Site Specific Functions */
-if (is_multisite())
-{
-    $site_id = get_current_blog_id();
-
-    switch ($site_id)
-    {
-        // O3D 
-        case '13':
-            require_once 'sites/o3d/functions.php';
-        break;
-	}
-
-}
+add_filter(
+    "redux/salient_redux/field/typography/custom_fonts",
+    "salient_redux_custom_fonts"
+);
