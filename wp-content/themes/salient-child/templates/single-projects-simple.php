41958d008e7dc9431de66aa4eb39aa98c32c6f39
Andrew Bringaze <abringaze@linuxfoundation.org>
2023-01-25 23:38:10 +0000

    test3

--- wp-content/themes/salient-child/templates/single-projects-simple.php
+++ wp-content/themes/salient-child/templates/single-projects-simple.php
@@ -27,8 +27,53 @@ get_header();
 	<div class="container main-content">
 
 		<div class="full-width-section">
-		<?php echo do_shortcode('[nectar_global_section id="7879"]'); ?>
+			<div class="row-bg-wrap">
+				<div class="inner-wrap">
+				<?php if ($projects_banner != null && $projects_banner_type == "Image") { ?>
+					<div class="row-bg projects-background background-image" style="background-image: url(<?php echo $projects_banner; ?>);"></div>
+				<?php } else { ?>
+					<div class="row-bg projects-background background-image" style="background-color: <?php echo $projects_banner_color; ?>"></div>
+				<?php } ?>
+				</div>
+			</div>
+				
+			<div class="hentry projects-heading">
+				<div class="col span_12 section-title projects-title">
+					<?php if ($projects_header == "Logo") {
+					$header = $projects_logo; ?>
+					<img src="<?php echo $header; ?>" />
+					<?php } if ($projects_header == "Secondary Logo") {
+					$header = $projects_secondary_logo; ?>
+					<img src="<?php echo $header; ?>" />
+					<?php } if ($projects_header != "Logo" && $projects_header != "Secondary Logo") { ?>
+					<h1 class="entry-title" style="color:#ffffff;"><?php the_title(); ?></h1>
+					<?php } 
+					if ($projects_excerpt != null && $projects_banner != null) { ?>		
+						<p class="projects-title-text white"> <?php echo $projects_excerpt; ?> </p>
+					<?php } 
+					else  { ?> <p class="projects-title-text"> <?php echo $projects_excerpt; ?> </p>
+					<?php } ?>
+				</div>
+			</div>
 		</div>
+
+
+
+
+
+
+
+
+
+
+		<?php echo do_shortcode('[nectar_global_section id="7879"]'); ?>
+	
+	
+	
+	
+	
+	
+	
 	</div>
 	<!--/container main-content-->
 </div>
