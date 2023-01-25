3628e61ee8f6172906a1c26b0d863edd77f7c722
Andrew Bringaze <abringaze@linuxfoundation.org>
2023-01-25 23:59:51 +0000

    projects icon patched

--- wp-content/themes/salient-child/templates/single-projects-simple.php
+++ wp-content/themes/salient-child/templates/single-projects-simple.php
@@ -56,15 +56,63 @@ get_header();
 				</div>
 			</div>
 		</div>
+		<!--/full-width-section -->
+		
+		<div class="row" style="padding-bottom: 0">
+			<div class="projects-post-area col" style="margin-bottom: 0">
+			
+				<div class="row">
+					<section class="projects-description">
+					<?php if ($projects_description_title != null || $projects_description_title != "")  { 
+						  echo "<h6 class='projects-description-title'>" . $projects_description_title . "</h6>"; 
+					} ?>	
+					<?php if ($projects_description != null || $projects_description != "") {
+					// Pull Nectar Slider CSS
+					wp_enqueue_style("nectar-slider");
+					wp_enqueue_style("font-awesome");
+					wp_enqueue_style("nectar-slider-fonts");
+					// Pull Nectar Slider JS
+					wp_enqueue_script("anime");
+					wp_enqueue_script("nectar-slider");
+					echo $projects_description; 
+					} ?>
+					</section>
+				</div>
 
 
 
-
-
-
-
-
-
+				<div class="full-width-section">
+					<div class="row-bg-wrap">
+						<div class="inner-wrap">
+							<?php if ($icon_banner != null) { 
+							$banner = "background: url(" . $icon_banner . ")"; } else {
+							$banner = "background-color: " . $accent_color;
+							} ?>				
+							<div class="row-bg projects-background" style="<?php echo $banner; ?>"></div>
+						</div>
+					</div>
+					
+					<section class="projects-links" style="margin-bottom: 0">
+						<?php if (have_rows("projects_icon_urls")):
+						while (have_rows("projects_icon_urls")):
+							the_row();
+							if (get_row_layout() == "projects_add_icon_url"):
+								$projects_url = get_sub_field("projects_url");
+								$projects_icon = get_sub_field("projects_icon");
+								$projects_icon_name = get_sub_field("projects_icon_name");
+								echo "<div class='projects-icons'>";
+								echo "<a class='projects-icon' href='" . $projects_url . "'>" . $projects_icon . "</a>" . "<p><a href='" . $projects_url . "'>" . $projects_icon_name . "</a></p>";
+								echo "</div>";
+							endif;
+						endwhile;
+					endif; ?>
+					</section>
+				</div>
+				<!--/full-width-section -->
+				
+			</div>
+			<!--/projects-post-area-->
+		</div>
 
 		<?php echo do_shortcode('[nectar_global_section id="7879"]'); ?>
 	
