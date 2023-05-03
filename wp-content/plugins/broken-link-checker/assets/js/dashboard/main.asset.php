614e5c049ac8d106f371fe8801ef60ff10715aca
Andrew Bringaze <abringaze@linuxfoundation.org>
2023-05-03 20:37:50 +0000

    Broken Link Checker Removal

--- wp-content/plugins/broken-link-checker/assets/js/dashboard/main.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '535b5099d408f9a65e03');
--- wp-content/plugins/broken-link-checker/assets/js/legacy-modal/main.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => 'f96fa56f88ef39ca38c7');
