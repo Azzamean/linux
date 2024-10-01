4b7a81511ba98bc46a7df21e788938bcdfd0a355
Andrew Bringaze <abringaze@linuxfoundation.org>
2024-10-01 15:49:35 +0000

    template js fix v2

--- wp-content/themes/salient-child/templates/js/templates.js
+++ wp-content/themes/salient-child/templates/js/templates.js
@@ -10,26 +10,20 @@ jQuery(document).ready(function ($) {
    $('[data-name="projects_category"]').hide();
    $('[data-name="projects_case_studies_title"]').hide();
    $('[data-name="projects_case_studies"]').hide();
-
+   $('[data-name="projects_video"]').show();
    if ($('#page_template :selected').text() == 'Default template') {
       $('[data-name="projects_header_excerpt"]').show();
-      $('[data-name="projects_video"]').show();
       $('[data-name="projects_post_category"]').show();
    } else if ($('#page_template :selected').text() == 'Template Style 1 (One)') {
-      $('[data-name="projects_video"]').hide();
    } else if ($('#page_template :selected').text() == 'Template Style 2 (Two)') {
-      $('[data-name="projects_video"]').hide();
-
    } else if ($('#page_template :selected').text() == 'Template Style 3 (Three)') {
-      $('[data-name="projects_video"]').hide();
       $('[data-name="projects_category"]').show();
       $('[data-name="projects_case_studies_title"]').show();
       $('[data-name="projects_case_studies"]').show();
    } else if ($('#page_template :selected').text() == 'Template Style 4 (Four)') {
-      $('[data-name="projects_video"]').show();
       $('[data-name="projects_post_category"]').show();
+      console.log(alert($('#page_template :selected').text()));
    }
-
 
 
    // WORKING GROUPS
@@ -39,12 +33,6 @@ jQuery(document).ready(function ($) {
    $('[data-name="working_groups_category"]').hide();
    $('[data-name="working_groups_case_studies_title"]').hide();
    $('[data-name="working_groups_case_studies"]').hide();
-
-
-   if ($('#page_template :selected').text() == 'Default template') {
-
-   }
-
    $('[data-name="webinars_speakers"]').hide();
 
 });
